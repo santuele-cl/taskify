78f6b331b397fb61777f97485747c3fe0910f013
santuele-cl <santuele.cl@gmail.com>
2024-01-31 20:51:26 +0800

    feat: create task input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Box, Container } from "@mui/material";
 import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <Box sx={{ bgcolor: "#2f74c0", minHeight: "100vh" }}>
+      <Container
+        maxWidth="lg"
+        sx={{
+          p: "20px",
+        }}
+      >
+        <Home />
+      </Container>
+    </Box>
+  );
 }
 
 export default App;
